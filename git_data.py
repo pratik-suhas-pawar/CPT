00a3f8b66a359a3ee0b6f0b800b703c999f50bd6
Pratik Pawar <117884284+pratik-suhas-pawar@users.noreply.github.com>
2023-03-28 23:58:33 +0530

    Update git_data.py

--- git_data.py
+++ git_data.py
@@ -8,6 +8,11 @@ class GitData:
         self._today = month_year[0] + " " + date + ", " + month_year[1]
         print(self._today)
 
-    
+    def get_today(self, usr_id: str = "") -> "Returns today's report":
+        data = str(requests.get(f"https://www.github.com/{usr_id}").content)
+        find_index = data.find(self._today)
+        contrib = str(data)[find_index - 35: find_index + 13].split(">")[-1].split(" ")[0]
+        if contrib == "NO":
+            return 0
 
 # print(GitData().get_today(usr_id="pratik-suhas-pawar"))
